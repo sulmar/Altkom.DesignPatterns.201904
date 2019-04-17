872461e0776f39b6ad1f49a671f63b1e0a9be681
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-04-17 17:14:27 +0200

    Maszyna stanów

---
+++ State/Lamp.cs
@@ -0,0 +1,71 @@
+﻿using Stateless;
+using Stateless.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    class Lamp
+    {
+        // Install-Package Stateless
+
+        private StateMachine<LampState, LampTrigger> machine;
+
+        public Lamp()
+        {
+            machine = new StateMachine<LampState, LampTrigger>(LampState.Off);
+
+            machine.Configure(LampState.Off)
+                .Permit(LampTrigger.Down, LampState.On)
+                .Ignore(LampTrigger.Up)
+                ;
+
+            machine.Configure(LampState.On)
+                .OnEntry(() => Console.WriteLine("Pamietaj o wyl. swiatla"), "Powitanie")
+                .Permit(LampTrigger.Up, LampState.Off)
+                .Permit(LampTrigger.Down, LampState.Blinking)
+               // .Ignore(LampTrigger.Down)
+                ;
+
+            machine.Configure(LampState.Blinking)
+                .Permit(LampTrigger.Up, LampState.Off)
+                .Permit(LampTrigger.Down, LampState.On);
+        }
+
+        public void PushDown() => machine.Fire(LampTrigger.Down);
+
+        public void PushUp() => machine.Fire(LampTrigger.Up);
+
+        public bool CanPushDown => machine.CanFire(LampTrigger.Down);
+
+        public bool CanPushUp => machine.CanFire(LampTrigger.Up);
+
+        public LampState State => machine.State;
+
+        public string Graph => UmlDotGraph.Format(machine.GetInfo());
+
+        public string SerialNumber { get; set; }
+
+        public Lamp(string serialNumber)
+            : this()
+        {
+            SerialNumber = serialNumber;
+        }
+    }
+
+    enum LampTrigger
+    {
+        Up,
+        Down
+    }
+
+    enum LampState
+    {
+        On,
+        Off,
+        Blinking
+    }
+}
---
+++ State/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LampTest();
+
+            // OrderTest();
+        }
+
+        private static void LampTest()
+        {
+            Lamp lamp = new Lamp("L001");
+
+            Console.WriteLine(lamp.Graph);
+            
+            Console.WriteLine(lamp.State);
+
+            if (lamp.CanPushDown)
+                lamp.PushDown();
+
+
+            Console.WriteLine(lamp.State);
+
+            lamp.PushDown();
+
+            Console.WriteLine(lamp.State);
+
+            if (lamp.CanPushUp)
+            lamp.PushUp();
+
+            Console.WriteLine(lamp.State);
+            if (lamp.CanPushUp)
+                lamp.PushUp();
+
+            Console.WriteLine(lamp.State);
+
+        }
+
+        private static void OrderTest()
+        {
+            Order order = new Order(1, "ZA 1/04/2019", 1000);
+            Warehouse(order);
+
+            Cancel(order);
+        }
+
+        private static void Cancel(Order order)
+        {
+            if (order.Status == OrderStatus.Created
+                || order.Status == OrderStatus.Completion)
+            {
+                order.Status = OrderStatus.Canceled;
+            }
+        }
+
+        private static void Warehouse(Order order)
+        {
+            if (order.Status == OrderStatus.Created)
+            {
+                order.Status = OrderStatus.Completion;
+
+                // ...               
+
+                order.Status = OrderStatus.Sent;
+
+            }
+        }
+    }
+
+    class Order
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+        public decimal TotalAmount { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public Order()
+        {
+            Status = OrderStatus.Created;
+        }
+
+        public Order(int id, string number, decimal totalAmount)
+            : this()
+        {
+            Id = id;
+            Number = number;
+            TotalAmount = totalAmount;
+        }
+    }
+
+    enum OrderStatus
+    {
+        Created,
+        Completion,
+        Sent,
+        Canceled,
+        Done
+    }
+}
