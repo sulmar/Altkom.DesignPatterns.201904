5c4d637f26ebf6e6acaf564de4edf29aa953a915
Marcin Sulecki <marcin.sulecki@gmail.com>
2019-04-19 10:10:55 +0200

    Dekorator

---
+++ Decorator/Models/Visit.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator.Models
+{
+    public class PrivateVisit : Visit
+    {
+
+    }
+
+    public class NFZVisit : Visit
+    {
+
+    }
+
+    public abstract class Visit
+    {
+        public Person Patient { get; set; }
+        public Person Doctor { get; set; }
+        public DateTime VisitDate { get; set; }
+        public TimeSpan Duration { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public decimal Amount
+        {
+            get
+            {
+                return (decimal)Duration.TotalHours * UnitPrice;
+            }
+        }
+
+        public Visit()
+        {
+            UnitPrice = 100;
+            Duration = TimeSpan.FromMinutes(15);
+        }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+    }
+
+    public enum Gender
+    {
+        Female,
+        Male
+    }
+
+    public struct Range<T>
+      where T : struct
+    {
+        public Range(T from, T to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public T From { get; set; }
+        public T To { get; set; }
+    }
+
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,155 @@
+﻿using Decorator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                FirstName = "Ann",
+                LastName = "Smith",
+                Gender = Gender.Female
+            };
+
+            Visit visit = new PrivateVisit
+            {
+                VisitDate = DateTime.Now,
+                Duration = TimeSpan.FromHours(2),
+                Patient = person
+            };
+
+
+            var period = new Range<TimeSpan>(TimeSpan.FromHours(8.5), TimeSpan.FromHours(15.5));
+            ICanDiscountStrategy canDiscountStrategy = new HappyHoursCanDiscountStrategy(period);
+            IDiscountStrategy discountStrategy = new FixedDiscountStrategy(20);
+
+
+            IVisitCalculator visitCalculator
+                = new DiscountDecorator(
+                                new HappyHoursCanDiscountStrategy(period),
+                                new FixedDiscountStrategy(20),                         
+                                    new DiscountDecorator(new GenderCanDiscountStrategy(Gender.Female),
+                                        new PercentageDiscountStrategy(0.5m), 
+                                        new VisitCalculator()));
+            
+            decimal totalAmount = visitCalculator.Calculate(visit);
+
+            Console.WriteLine($"Total amount: {totalAmount:C2}");
+        }
+    }
+
+    public interface ICanDiscountStrategy
+    {
+        bool CanDiscount(Visit visit);
+    }
+
+    public interface IDiscountStrategy
+    {
+        decimal Discount(Visit visit);
+    }
+
+    public interface IVisitCalculator
+    {
+        decimal Calculate(Visit visit);
+    }
+
+    public class VisitCalculator : IVisitCalculator
+    {
+        public decimal Calculate(Visit visit)
+        {
+            return visit.Amount;
+        }
+    }
+
+    class HappyHoursCanDiscountStrategy : ICanDiscountStrategy
+    {
+        private readonly Range<TimeSpan> period;
+
+        public HappyHoursCanDiscountStrategy(Range<TimeSpan> period)
+        {
+            this.period = period;
+        }
+
+        public bool CanDiscount(Visit visit)
+        {
+            return visit.VisitDate.TimeOfDay >= period.From
+               && visit.VisitDate.TimeOfDay <= period.To;
+        }
+    }
+
+    public class GenderCanDiscountStrategy : ICanDiscountStrategy
+    {
+        private readonly Gender gender;
+
+        public GenderCanDiscountStrategy(Gender gender) => this.gender = gender;
+
+        public bool CanDiscount(Visit visit) => visit.Patient.Gender == gender;
+    }
+
+
+    class FixedDiscountStrategy : IDiscountStrategy
+    {
+        private readonly decimal discount;
+
+        public FixedDiscountStrategy(decimal discount)
+        {
+            this.discount = discount;
+        }
+
+        public decimal Discount(Visit visit)
+        {
+            return visit.Amount - discount;
+        }
+    }
+
+    public class PercentageDiscountStrategy : IDiscountStrategy
+    {
+        private readonly decimal percentage;
+
+        public PercentageDiscountStrategy(decimal percentage)
+        {
+            this.percentage = percentage;
+        }
+
+        public decimal Discount(Visit visit)
+        {
+            return visit.Amount * percentage;
+        }
+    }
+
+
+    public class DiscountDecorator : IVisitCalculator
+    {
+        private readonly IVisitCalculator calculator;
+        private readonly ICanDiscountStrategy canDiscountStrategy;
+        private readonly IDiscountStrategy discountStrategy;
+
+        public DiscountDecorator(ICanDiscountStrategy canDiscountStrategy, IDiscountStrategy discountStrategy, IVisitCalculator calculator)
+        {
+            this.calculator = calculator;
+            this.canDiscountStrategy = canDiscountStrategy;
+            this.discountStrategy = discountStrategy;
+        }
+
+        public decimal Calculate(Visit visit)
+        {
+            decimal discount = calculator.Calculate(visit);
+            
+            if (canDiscountStrategy.CanDiscount(visit))
+            {
+                discount -= discountStrategy.Discount(visit);
+            }
+
+            return discount;
+        }
+    }
+
+    
+}
